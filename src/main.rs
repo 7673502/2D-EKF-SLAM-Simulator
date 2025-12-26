05e73e81e39bbf57080d006e97e0aba5e49e72d1
Pramod Anandarao <panandarao@wisc.edu>
2025-12-26 12:06:35 -0600

    reorganized project structure

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use macroquad::prelude::*;
-use ::rand::rng;
-use rand_distr::{Normal, Distribution};
 
+mod geometry;
 mod config;
+mod simulation;
 use config::Config;
+
 
 fn window_conf() -> Conf {
     Conf {
@@ -15,66 +16,43 @@ fn window_conf() -> Conf {
     }
 }
 
-// converts ground truth x and y to graphical coordinates
-fn gt_to_screen(x: f32, y: f32) -> (f32, f32) {
-    (screen_width() / 4.0 + x, screen_height() / 2.0 - y)
-}
-
-// converts graphical x and y to ground truth
-fn screen_to_gt(x: f32, y: f32) -> (f32, f32) {
-    (x - screen_width() / 4.0, screen_height() / 2.0 - y)
-}
-
 #[macroquad::main(window_conf)]
 async fn main() {
     let cfg = Config::default();
-
-    // position states
-    let mut gt_x: f32 = 0.0;
-    let mut gt_y: f32 = 0.0; 
-    let mut gt_linear_velocity: f32 = 0.0;
-
-    // direction states
-    let mut gt_dir: f32 = 0.0;
-    let mut gt_angular_velocity: f32 = 0.0;
-    
-    // noise generation
-    let mut rng = rng();
-    let normal = Normal::new(0.0, 1.0).unwrap();
-    let mut epsilon = |alpha: f32, velocity: f32| alpha * velocity * normal.sample(&mut rng);
-    
-    let mut prev_gt_linear_velocity: f32 = 0.0;
-    let mut prev_gt_angular_velocity: f32 = 0.0;
     
     // rectangles and landmarks
     let mut obstructions: Vec<Rect> = Vec::new();
     let mut landmarks: Vec<(f32, f32)> = Vec::new();
 
+    let mut robot = simulation::Robot::new();
 
     loop {
         clear_background(BLACK);
         
         let delta_time: f32 = get_frame_time();
-        let (effective_gt_x, effective_gt_y) = gt_to_screen(gt_x, gt_y);
+        let (effective_gt_x, effective_gt_y) = geometry::gt_to_screen(robot.x, robot.y);
 
         // movement
         if is_key_down(KeyCode::Up) {
-            gt_linear_velocity += cfg.linear_acc * delta_time;
+            robot.linear_velocity += cfg.linear_acc * delta_time;
         }
         if is_key_down(KeyCode::Down) {
-            gt_linear_velocity -= cfg.linear_acc * delta_time;
+            robot.linear_velocity -= cfg.linear_acc * delta_time;
         }
         if is_key_down(KeyCode::Right) {
-            gt_angular_velocity -= cfg.angular_acc * delta_time;
+            robot.angular_velocity -= cfg.angular_acc * delta_time;
         }
         if is_key_down(KeyCode::Left) {
-            gt_angular_velocity += cfg.angular_acc * delta_time;
+            robot.angular_velocity += cfg.angular_acc * delta_time;
         }
+        
+        robot.update(delta_time, &cfg, &obstructions);
         
         // adding landmarks and obstructions
         let effective_mouse_x: f32 = mouse_position().0;
         let effective_mouse_y: f32 = mouse_position().1;
-        let (mouse_x, mouse_y) = screen_to_gt(effective_mouse_x, effective_mouse_y);
+        let (mouse_x, mouse_y) = geometry::screen_to_gt(effective_mouse_x, effective_mouse_y);
+
         if is_mouse_button_released(MouseButton::Left) {
             // delete the obstruction if mouse is touching it
             let mut removed = false;
@@ -110,73 +88,27 @@ async fn main() {
                 landmarks.push((mouse_x, mouse_y));
             }
         }
-        
-        // bound velocity
-        gt_linear_velocity = gt_linear_velocity.clamp(-cfg.max_linear_speed, cfg.max_linear_speed);
-        gt_angular_velocity = gt_angular_velocity.clamp(-cfg.max_angular_speed, cfg.max_angular_speed);
-
-        // apply decay
-        gt_linear_velocity *= cfg.decay_factor.powf(delta_time);
-        gt_angular_velocity *= cfg.decay_factor.powf(delta_time);
-        
-        let gt_linear_velocity: f32 = gt_linear_velocity + epsilon(cfg.alpha_linear, gt_linear_velocity.abs());
-        let gt_angular_velocity: f32 = gt_angular_velocity + epsilon(cfg.alpha_angular, gt_angular_velocity.abs());
-
-        // update direction
-        gt_dir += 0.5 * (gt_angular_velocity + prev_gt_angular_velocity) * delta_time;
-        gt_dir = (gt_dir + 2.0 * std::f32::consts::PI) % (2.0 * std::f32::consts::PI);
-        
-        // update position
-        gt_x += (0.5 * (gt_linear_velocity + prev_gt_linear_velocity) * delta_time) * gt_dir.cos();
-        gt_y += (0.5 * (gt_linear_velocity + prev_gt_linear_velocity) * delta_time) * gt_dir.sin();
-        gt_x = gt_x.clamp(cfg.robot_radius - screen_width() / 4.0, screen_width() / 4.0 - cfg.robot_radius);
-        gt_y = gt_y.clamp(cfg.robot_radius - screen_height() / 2.0, screen_height() / 2.0 - cfg.robot_radius);
-
-        // detect obstruction
-        for obstruction in obstructions.iter() {
-            let closest_x = gt_x.clamp(obstruction.x - obstruction.w / 2.0, obstruction.x + obstruction.w / 2.0);
-            let closest_y = gt_y.clamp(obstruction.y - obstruction.h / 2.0, obstruction.y + obstruction.h / 2.0);
-            
-            // distance from closest point on obstruction to center of robot
-            let distance_x = gt_x - closest_x;
-            let distance_y = gt_y - closest_y;
-            let distance_sq = distance_x * distance_x + distance_y * distance_y;
-            
-            if distance_sq < cfg.robot_radius * cfg.robot_radius {
-                let distance = distance_sq.sqrt();
-                
-                if distance > 0.0 {
-                    gt_x = closest_x + cfg.robot_radius * (distance_x / distance);
-                    gt_y = closest_y + cfg.robot_radius * (distance_y / distance);
-                }
-            }
-        }
 
         // draw obstructions and landmarks
         for obstruction in obstructions.iter() {
-            let (effective_rect_x, effective_rect_y) = gt_to_screen(obstruction.x - obstruction.w / 2.0, obstruction.y + obstruction.h / 2.0);
+            let (effective_rect_x, effective_rect_y) = geometry::gt_to_screen(obstruction.x - obstruction.w / 2.0, obstruction.y + obstruction.h / 2.0);
             draw_rectangle(effective_rect_x, effective_rect_y, obstruction.w, obstruction.h, GRAY);
         }
         for landmark in landmarks.iter() {
-            let (effective_landmark_x, effective_landmark_y) = gt_to_screen(landmark.0, landmark.1);
+            let (effective_landmark_x, effective_landmark_y) = geometry::gt_to_screen(landmark.0, landmark.1);
             draw_circle(effective_landmark_x, effective_landmark_y, cfg.landmark_radius, RED);
         }
 
         // draw "robot"; segment shows direction
         draw_circle(effective_gt_x, effective_gt_y, cfg.robot_radius, BLUE);
-        draw_line(effective_gt_x, effective_gt_y, effective_gt_x + cfg.robot_radius * gt_dir.cos(), effective_gt_y - cfg.robot_radius * gt_dir.sin(), 4.0, WHITE);
+        draw_line(effective_gt_x, effective_gt_y, effective_gt_x + cfg.robot_radius * robot.dir.cos(), effective_gt_y - cfg.robot_radius * robot.dir.sin(), 4.0, WHITE);
 
         // ground truth text
-        draw_text(&format!("pos: ({:.0}, {:.0})", gt_x, gt_y), 25.0, 50.0, 36.0, WHITE);
-        draw_text(&format!("angle: {:.2} rad", gt_dir), 25.0, 100.0, 36.0, WHITE);
+        draw_text(&format!("pos: ({:.0}, {:.0})", robot.x, robot.y), 25.0, 50.0, 36.0, WHITE);
+        draw_text(&format!("angle: {:.2} rad", robot.dir), 25.0, 100.0, 36.0, WHITE);
         
         // dividing line between ground truth world and robot's perceived world
         draw_line(screen_width() / 2.0, 0.0, screen_width() / 2.0, screen_height(), 4.0, WHITE);
-
-
-        // needed for calculating x, y, and dir on next frame
-        prev_gt_linear_velocity = gt_linear_velocity;
-        prev_gt_angular_velocity = gt_angular_velocity;
 
         next_frame().await
     }
---
+++ src/simulation.rs
@@ -0,0 +1,80 @@
+use macroquad::prelude::*;
+use rand_distr::{Normal, Distribution};
+use ::rand::{rng, rngs::ThreadRng};
+
+use crate::config::Config;
+
+pub struct Robot {
+    pub x: f32,
+    pub y: f32,
+    pub dir: f32,
+    pub linear_velocity: f32,
+    pub angular_velocity: f32,
+    pub prev_linear_velocity: f32,
+    pub prev_angular_velocity: f32,
+    rng: ThreadRng,
+    normal: Normal<f32>,
+}
+
+impl Robot {
+    pub fn new() -> Self {
+        Self {
+            x: 0.0,
+            y: 0.0,
+            dir: 0.0,
+            linear_velocity: 0.0,
+            angular_velocity: 0.0,
+            prev_linear_velocity: 0.0,
+            prev_angular_velocity: 0.0,
+            rng: rng(),
+            normal: Normal::new(0.0, 1.0).unwrap(),
+        }
+    }
+
+    pub fn update(&mut self, delta_time: f32, cfg: &Config, obstructions: &[Rect]) {
+        // bound velocity
+        self.linear_velocity = self.linear_velocity.clamp(-cfg.max_linear_speed, cfg.max_linear_speed);
+        self.angular_velocity = self.angular_velocity.clamp(-cfg.max_angular_speed, cfg.max_angular_speed);
+
+        // apply decay
+        self.linear_velocity *= cfg.decay_factor.powf(delta_time);
+        self.angular_velocity *= cfg.decay_factor.powf(delta_time);
+        
+        self.linear_velocity = self.linear_velocity + cfg.alpha_linear  * self.linear_velocity.abs() * self.normal.sample(&mut self.rng);
+        self.angular_velocity = self.angular_velocity + cfg.alpha_angular * self.angular_velocity.abs() * self.normal.sample(&mut self.rng);
+
+        // update direction
+        self.dir += 0.5 * (self.angular_velocity + self.prev_angular_velocity) * delta_time;
+        self.dir = (self.dir + 2.0 * std::f32::consts::PI) % (2.0 * std::f32::consts::PI);
+        
+        // update position
+        self.x += (0.5 * (self.linear_velocity + self.prev_linear_velocity) * delta_time) * self.dir.cos();
+        self.y += (0.5 * (self.linear_velocity + self.prev_linear_velocity) * delta_time) * self.dir.sin();
+        self.x = self.x.clamp(cfg.robot_radius - screen_width() / 4.0, screen_width() / 4.0 - cfg.robot_radius);
+        self.y = self.y.clamp(cfg.robot_radius - screen_height() / 2.0, screen_height() / 2.0 - cfg.robot_radius);
+
+        // detect obstruction
+        for obstruction in obstructions.iter() {
+            let closest_x = self.x.clamp(obstruction.x - obstruction.w / 2.0, obstruction.x + obstruction.w / 2.0);
+            let closest_y = self.y.clamp(obstruction.y - obstruction.h / 2.0, obstruction.y + obstruction.h / 2.0);
+            
+            // distance from closest point on obstruction to center of robot
+            let distance_x = self.x - closest_x;
+            let distance_y = self.y - closest_y;
+            let distance_sq = distance_x * distance_x + distance_y * distance_y;
+            
+            if distance_sq < cfg.robot_radius * cfg.robot_radius {
+                let distance = distance_sq.sqrt();
+                
+                if distance > 0.0 {
+                    self.x = closest_x + cfg.robot_radius * (distance_x / distance);
+                    self.y = closest_y + cfg.robot_radius * (distance_y / distance);
+                }
+            }
+        } 
+
+        // needed for calculating x, y, and dir on next frame
+        self.prev_linear_velocity = self.linear_velocity;
+        self.prev_angular_velocity = self.angular_velocity;
+    }
+}
